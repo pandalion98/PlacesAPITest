ba89b77896d9660c188f46396ed9c56a1a472ec3
pandalion98 <pandalion98@gmail.com>
2019-02-18 15:36:17 +0800

    Working address box!

--- app/src/main/java/ph/kirig/placesapitest/FirstTestActivity.java
+++ app/src/main/java/ph/kirig/placesapitest/FirstTestActivity.java
@@ -1,13 +1,17 @@
 package ph.kirig.placesapitest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class FirstTest extends AppCompatActivity {
+public class FirstTestActivity extends AppCompatActivity {
+
+    private DelayAutoCompleteTextView mAddressBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_test);
+
+        mAddressBox = findViewById(R.id.edittext_address);
     }
 }
---
+++ app/src/main/java/ph/kirig/placesapitest/PlacesAutoCompleteAdapter.java
@@ -0,0 +1,9 @@
+package ph.kirig.placesapitest;
+
+/**
+ * Created by Gene on 18/02/2019.
+ * Kirig Technologies
+ * gene(at)kirig.ph
+ */
+public class PlacesAutoCompleteAdapter {
+}
